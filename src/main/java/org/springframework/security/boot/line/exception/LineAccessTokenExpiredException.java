04d5adafb4b9288f5ef3b74b90e3110cc34ea10b
loong10k <20489781+loong10k@users.noreply.github.com>
2026-08-10 16:59:10 +0800

    docs: unify @author tag to @author <a href="https://github.com/loong10k">Loong Wan</a>

--- src/main/java/org/springframework/security/boot/line/exception/LineAccessTokenExpiredException.java
+++ src/main/java/org/springframework/security/boot/line/exception/LineAccessTokenExpiredException.java
@@ -19,7 +19,7 @@ import org.springframework.security.boot.biz.exception.AuthenticationTokenExpire
 
 /**
  * Line AccessToken Expired Exception
- * @author [@Loong Wan](https://github.com/loong10k)
+ * @author <a href="https://github.com/loong10k">Loong Wan</a>
  */
 @SuppressWarnings("serial")
 public class LineAccessTokenExpiredException extends AuthenticationTokenExpiredException {
