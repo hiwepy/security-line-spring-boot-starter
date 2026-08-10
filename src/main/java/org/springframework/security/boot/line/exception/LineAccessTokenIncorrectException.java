9cdf858794237d630ff2c2fe5dc52d1897e695f4
loong10k <20489781+loong10k@users.noreply.github.com>
2026-08-10 16:59:07 +0800

    docs: unify @author tag to @author <a href="https://github.com/loong10k">Loong Wan</a>

--- src/main/java/org/springframework/security/boot/line/exception/LineAccessTokenIncorrectException.java
+++ src/main/java/org/springframework/security/boot/line/exception/LineAccessTokenIncorrectException.java
@@ -19,7 +19,7 @@ import org.springframework.security.boot.biz.exception.AuthenticationTokenIncorr
 
 /**
  * Line AccessToken Incorrect Exception
- * @author [@Loong Wan](https://github.com/loong10k)
+ * @author <a href="https://github.com/loong10k">Loong Wan</a>
  */
 @SuppressWarnings("serial")
 public class LineAccessTokenIncorrectException extends AuthenticationTokenIncorrectException {
